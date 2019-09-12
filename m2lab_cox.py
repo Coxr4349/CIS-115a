893fe37f398c3ef0b904d0bbf4de2f4f1a40b83a
Coxr4349 <55244488+Coxr4349@users.noreply.github.com>
2019-09-12 10:43:47 -0400

    First assignment

---
+++ m2lab_cox.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+M2T1
+Cox
+9/12/19
+"""
+
+# convert M2T1 into Python
+
+#// M2LAB
+#// Ryan Cox
+#// 9/12/19
+
+#// Greet user by name,
+#// tell them their age
+#// on their next birthday
+
+#// Deaclartions
+#Deaclare String name
+#Deaclare Interger age
+
+#// Ask for name
+#Display "What is your name?"
+print("What is your name?")
+#Input name
+name = input ()
+
+#// Ask for age
+#Display "How old are you?"
+print("How old are you?")
+#Input age
+age = int(input())
+
+#// Tell them (by name) how old they will be
+#// on their next birthday
+#Set nextAge = age + 1
+nextAge = age + 1
+#Display "You will be ", nextAge, " years old on your next birthday, ", name, "."
+print("You will be ", nextAge, " years old on your next birthday, ", name, ".")
